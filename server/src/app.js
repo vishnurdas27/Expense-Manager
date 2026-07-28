02a6fd5c2bbd9c4c796ec993d6eae71a90411410
vishnurdas27 <vishnurdas27@gmail.com>
2026-07-28 23:42:20 +0530

    app js updates

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import authRouter from './modules/auth/auth.routes'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/api/health',(req, res)=>{
+    res.json({status: 'ok'})
+})
+
+app.use('/auth', authRouter);
+
+export default app
